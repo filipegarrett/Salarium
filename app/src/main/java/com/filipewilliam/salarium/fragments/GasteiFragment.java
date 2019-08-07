10476a364a3ab590e1a6f201471e4113088a1eff
William <william_senoski@hotmail.com>
2019-08-07 19:53:23 -0300

    implementei a tela de gastos

--- app/src/main/java/com/filipewilliam/salarium/fragments/GasteiFragment.java
+++ app/src/main/java/com/filipewilliam/salarium/fragments/GasteiFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 import com.filipewilliam.salarium.R;
 
@@ -13,6 +14,7 @@ import com.filipewilliam.salarium.R;
  * A simple {@link Fragment} subclass.
  */
 public class GasteiFragment extends Fragment {
+
 
 
     public GasteiFragment() {
@@ -23,6 +25,7 @@ public class GasteiFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_gastei, container, false);
     }
