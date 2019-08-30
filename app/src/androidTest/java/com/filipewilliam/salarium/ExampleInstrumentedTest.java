28638f6a1175f3b604734a98252511efa066f38d
wowbaggerBR <filipe.garrett@gmail.com>
2019-08-30 07:29:58 -0300

    Migração para Android.support. O arquivo google-services.json, referente à conexão com Firebase, não está versionado nesse commit

--- app/src/androidTest/java/com/filipewilliam/salarium/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/filipewilliam/salarium/ExampleInstrumentedTest.java
@@ -1,14 +1,13 @@
 package com.filipewilliam.salarium;
 
 import android.content.Context;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,7 +19,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.filipewilliam.salarium", appContext.getPackageName());
     }
