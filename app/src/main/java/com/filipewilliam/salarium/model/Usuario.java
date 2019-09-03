a50c7831da4d6c269fdd8d6da308e1fd189faf26
wowbaggerBR <filipe.garrett@gmail.com>
2019-09-03 15:39:09 -0300

    - Implementação dos slides de introdução que substituem o splash screen - Implementação do Firebase Auth: já é permitido criar usuários (embora seja preciso tratar exceções de senhas fracas) - Inúmeras correções de posição, cor e ancoragem de elementos de interface - Inclusão de itens, ilustrações e ícones

---
+++ app/src/main/java/com/filipewilliam/salarium/model/Usuario.java
@@ -0,0 +1,41 @@
+package com.filipewilliam.salarium.model;
+
+public class Usuario {
+
+    String nome, dataNascimento, email, senha;
+
+    public Usuario() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
