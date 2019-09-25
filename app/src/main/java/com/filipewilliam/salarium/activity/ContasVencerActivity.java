4c6f3fed582e7c8bea8f639b11d9f7d6e9782fd2
Filipe de Almeida Garrett <filipe.garrett@gmail.com>
2019-09-25 17:52:36 -0300

    - Ajustes de interface gráfica nos componentes da ContasVencerActivity

--- app/src/main/java/com/filipewilliam/salarium/activity/ContasVencerActivity.java
+++ app/src/main/java/com/filipewilliam/salarium/activity/ContasVencerActivity.java
@@ -3,10 +3,23 @@ package com.filipewilliam.salarium.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.filipewilliam.salarium.R;
+import com.filipewilliam.salarium.helpers.DatasMaskWatcher;
+import com.filipewilliam.salarium.helpers.ValoresEmReaisMaskWatcher;
 
 public class ContasVencerActivity extends AppCompatActivity {
+
+    private Spinner spinnerCategoriaContas;
+    private Button buttonLimparCamposContasVencer, buttonCadastrarContasVencer;
+    private EditText editTextValorContasVencer, editTextDataVencimentoContasVencer;
+    private Switch switchVencimentoRecorrente, switchEmitirNotificacaoVencimento;
+    DatasMaskWatcher maskWatcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +27,14 @@ public class ContasVencerActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Próximas despesas");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contas_vencer);
+
+        spinnerCategoriaContas = findViewById(R.id.spinnerCategoriaContasVencer);
+        editTextValorContasVencer = findViewById(R.id.editTextValorContasVencer);
+        editTextValorContasVencer.addTextChangedListener(new ValoresEmReaisMaskWatcher(editTextValorContasVencer));
+        editTextDataVencimentoContasVencer = findViewById(R.id.editTextDataVencimentoContaVencer);
+        editTextDataVencimentoContasVencer.addTextChangedListener(maskWatcher.aplicarMaskDataNascimento());
+        switchVencimentoRecorrente = findViewById(R.id.switchDespesaRecorrente);
+        switchEmitirNotificacaoVencimento = findViewById(R.id.switchPermitirNotificacoes);
     }
 
     @Override
